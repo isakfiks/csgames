bcebfa187bcf59eaee08e4cd7eb945b994fbd67a
Isak Fiksdal <106451308+isakfiks@users.noreply.github.com>
2025-05-09 10:24:20 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1>CS Games</h1>
+        <button className="border-black p-2 rounded-lg border-2">Get started</button>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-
       </footer>
     </div>
   );
