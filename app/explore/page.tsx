8198e8e20041ed994658a047a6d87fdd7e5c481a
Isak Fiksdal <106451308+isakfiks@users.noreply.github.com>
2025-05-09 14:12:26 +0200

    Update page.tsx

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 import Link from "next/link";
-import { FaInfoCircle, FaCompass  } from "react-icons/fa";
+import { FaGamepad, FaCompass  } from "react-icons/fa";
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { useState, useEffect } from 'react';
+
 
 const supabase = createClientComponentClient();
 
@@ -10,8 +10,18 @@ export default function Home() {
   return (
     <div className="bg-white grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="text-black text-center flex flex-col row-start-2 items-center">
-        <div className="flex">
-            <h1>Explore Games</h1>
+        <div>
+            <h1 className="mb-8">Explore Games</h1>
+
+            <div className="justify-center flex bg-gradient-to-r from-stone-800 to-stone-900 rounded-lg h-84 w-80">
+              <div className="flex flex-col justify-end items-center h-full w-full">
+                <h1 className="text-white mb-4">Clapping Bird Duels</h1>
+                <div className="justify-center items-center bg-white rounded-lg p-2 mb-8 flex">
+                  <FaGamepad className="w-8 mr-1"></FaGamepad>
+                <button className="">Create Lobby</button>
+              </div>
+              </div>
+            </div>
             </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
