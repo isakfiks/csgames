6c02a7799eec6b4335ef80c27b0af657a1c8be1a
Isak Fiksdal <106451308+isakfiks@users.noreply.github.com>
2025-05-09 13:17:06 +0200

    Fix colors

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -8,8 +8,8 @@ const supabase = createClientComponentClient();
 
 export default function Home() {
   return (
-    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="text-center flex flex-col row-start-2 items-center">
+    <div className="bg-white grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="text-black text-center flex flex-col row-start-2 items-center">
         <div className="flex">
             <h1>Explore Games</h1>
             </div>
