029c53df1b80f1025617d4c7e576d33a0e292ba9
Isak Fiksdal <106451308+isakfiks@users.noreply.github.com>
2025-05-09 13:06:47 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { FaInfoCircle, FaCompass  } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="text-center flex flex-col row-start-2 items-center">
+    <div className="bg-white grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="text-black text-center flex flex-col row-start-2 items-center">
         <div className="flex">
         <h1 className="text-2xl font-bold">CSGames</h1><span className="text-2xl mb-2">.dev</span>
         </div>
