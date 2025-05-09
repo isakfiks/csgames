e0bf2523726f77c3a63394ba7f6e713c83840b07
Isak Fiksdal <106451308+isakfiks@users.noreply.github.com>
2025-05-09 10:43:43 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="text-center flex flex-col gap-[32px] row-start-2 items-center">
         <h1>CS Games</h1>
-        <button className="border-black p-2 rounded-lg border-2">Explore Games</button>
+        <Link href="/explore">
+        <button className="border-black p-2 rounded-lg border-2">Explore Games</button></Link>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
